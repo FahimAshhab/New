e1b0b67c799340f7bc4cf8e5498506fe85300f15
FahimAshhab <f.ashhab@shiftboolean.com>
2023-08-10 03:07:14 +0600

    jhsd

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -23,7 +23,7 @@ export default function Index() {
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
+            15m Quickstart Blog Tutorialll
           </a>
         </li>
         <li>
